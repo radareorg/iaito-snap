211685e992ed3cfaba1c0a4ad977e3e371df48ab
Pau RE <prodrigestivill@gmail.com>
2024-09-02 17:07:42 +0200

    Switch to build all

--- .github/scripts/update-versions.sh
+++ .github/scripts/update-versions.sh
@@ -10,6 +10,19 @@ getLatestReleaseTag() {
 }
 
 IAITO_VERSION=$(getLatestReleaseTag radareorg/iaito)
+R2_VERSION=$(getLatestReleaseTag radareorg/radare2)
+R2GHIDRA_VERSION=$(getLatestReleaseTag radareorg/r2ghidra)
+R2FRIDA_VERSION=$(getLatestReleaseTag nowsecure/r2frida)
+R2DEC_VERSION=$(getLatestReleaseTag wargio/r2dec-js)
+YARA_VERSION=$(getLatestReleaseTag VirusTotal/yara)
+R2YARA_VERSION=$(getLatestReleaseTag radareorg/r2yara)
 
 echo "Updating versions in snap/snapcraft.yaml..." > /dev/stderr
-yq eval -i '.parts.iaito.source-tag=strenv(IAITO_VERSION)' snap/snapcraft.yaml
+yq eval -i '.parts.iaito.source-tag=strenv(IAITO_VERSION) |
+  .parts.radare2.source-tag=strenv(R2_VERSION) | 
+  .parts.r2ghidra.source-tag=strenv(R2GHIDRA_VERSION) |
+  .parts.r2frida.source-tag=strenv(R2FRIDA_VERSION) |
+  .parts.r2dec.source-tag=strenv(R2DEC_VERSION) |
+  .parts.yara.source-tag=strenv(YARA_VERSION) |
+  .parts.r2yara.source-tag=strenv(R2YARA_VERSION)
+  ' snap/snapcraft.yaml
