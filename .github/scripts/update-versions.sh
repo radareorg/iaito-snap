077fdca82f6406a4ba742b74c2114b268293ce9e
Pau RE <prodrigestivill@gmail.com>
2024-09-02 14:44:51 +0200

    Add initial github actions to update version

---
+++ .github/scripts/update-versions.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -ae
+
+cd "$(dirname "$0")/../.."
+
+getLatestReleaseTag() {
+  echo "Checking latest release version for $1..." > /dev/stderr
+  gh release view --repo "$1" --json tagName --template '{{.tagName}}'
+}
+
+IAITO_VERSION=$(getLatestReleaseTag radareorg/iaito)
+
+echo "Updating versions in snap/snapcraft.yaml..." > /dev/stderr
+yq eval -i '.version=strenv(IAITO_VERSION)' snap/snapcraft.yaml
